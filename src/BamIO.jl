f8173a36ddc70ec57693688063ae527bf1e3c134
christopher.schroeder@tu-dortmund.de <christopher.schroeder@tu-dortmund.de>
2016-03-03 15:38:26 +0100

    export flag functions

--- src/BamIO.jl
+++ src/BamIO.jl
@@ -1,5 +1,7 @@
 module BamIO
-export refid, position, l_read_name, mq, n_cigar_op, flag, l_seq, next_refid, next_position, tlen, read_name, cigar, seq, qualities, eof, nextread, reference_name, is_duplicate, is_unmapped
+export refid, position, l_read_name, mq, n_cigar_op, flag, l_seq, next_refid, next_position, tlen, read_name, cigar, seq, qualities, eof, nextread, reference_name
+export is_paired, is_proper_paired, is_unmapped, mate_unmapped, is_reverse, mate_reverse, is_read1, is_read2, not_primary, fail_qc, is_duplicate, supplementary_alignment
+
 include("bam.jl")
 
 # package code goes here
